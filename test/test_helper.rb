5e91e3c172bc07ccfcf42c4cd15dfb07afe0596b
buithanhcong <thanhcong.xyz@gmail.com>
2018-06-10 16:47:50 +0700

    Finish layout and routes

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,3 +1,4 @@
+ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
 
@@ -6,4 +7,6 @@ class ActiveSupport::TestCase
   fixtures :all
 
   # Add more helper methods to be used by all tests here...
+  include ApplicationHelper
+
 end
